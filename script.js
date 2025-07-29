049230c79e3d7af882753211bf2b503195679b09
Anjali gupta <anjaligupta225196@gmail.com>
2025-07-29 22:19:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let button = document.querySelector(".click");
+let audio = document.getElementById("audio");
+
+button.addEventListener("click", () => {
+    // audio.loop=tr;
+    audio.play();
+});
